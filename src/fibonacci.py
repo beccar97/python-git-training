1dc41f4b13a0da1aafae71b06e9c809e01e5a91d
Becky Carter <becky.carter@softwire.com>
2025-01-21 14:55:34 +0000

    allow optional negative recursive approach

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,4 +1,7 @@
-def compute_fibonacci_number(position: int) -> int:
+def compute_fibonacci_number(position: int, is_recursive: bool = False) -> int:
+    if is_recursive:
+        return recursive_fibonacci(position)
+
     i = 1
     j = 1
 
@@ -13,3 +16,22 @@ def compute_fibonacci_number(position: int) -> int:
         current_position += 1
 
     return j
+
+
+def recursive_fibonacci(
+    initial_position: int, left: int = 0, right: int = 1, position: int = None
+) -> int:
+    current_position = position if position is not None else initial_position
+
+    if initial_position == 0:
+        return 0
+    if current_position == 0:
+        return left
+    if initial_position > 0:
+        return recursive_fibonacci(
+            initial_position, right, left + right, current_position - 1
+        )
+    else:
+        return recursive_fibonacci(
+            initial_position, right - left, left, current_position + 1
+        )
